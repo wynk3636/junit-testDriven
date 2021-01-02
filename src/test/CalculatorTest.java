cd38f9e846fe3b680e8322ccb89dee5a4fc71ba3
ishida <hand3636hand@gmail.com>
2021-01-02 19:38:51 +0900

    add test code

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,5 +1,15 @@
 package test;
 
-public class CalculatorTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void add3and4() throws Exception {
+		Calculator sut = new Calculator();
+		assertThat(sut.add(3,4), is(7));
+	}
 }
